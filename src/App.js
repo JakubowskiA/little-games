5199dc0e172d18e49e23f0bf2d8a17be108ec2d4
Ariel Jakubowski <arieljakubowski@gmail.com>
2020-12-30 18:24:00 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -5,23 +5,8 @@ function App() {
   return (
     <Fragment>
       <nav>
-        <div className="nav-item hvr-grow">
+        <div>
           <a href="#about-me">About Me</a>
-        </div>
-        <div className="nav-item hvr-grow">
-          <a href="#experience">Experience</a>
-        </div>
-        <div className="nav-item hvr-grow">
-          <a href="#projects">Projects</a>
-        </div>
-        <div className="nav-item hvr-grow">
-          <a href="#blog">Blog</a>
-        </div>
-        <div className="nav-item hvr-grow">
-          <a href="#skills">Skills</a>
-        </div>
-        <div className="nav-item hvr-grow">
-          <a href="#contact">Contact</a>
         </div>
       </nav>
     </Fragment>
