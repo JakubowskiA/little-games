a946f35eb09e594e8e148ca83679ce0cc7e45e9c
Ariel Jakubowski <arieljakubowski@gmail.com>
2021-01-16 10:23:14 -0500

    Can't overwrite and styling changes

--- src/applications/tic-tac-toe/Board.js
+++ src/applications/tic-tac-toe/Board.js
@@ -1,34 +1,43 @@
 import React, { Component, Fragment, useState } from "react";
-import {useSelector} from 'react-redux'
-import Square from './Square'
+import { useSelector } from "react-redux";
+import Square from "./Square";
 
 const Board = (props) => {
-    const [boardInfo, setBoardInfo] = useState(useSelector(state => state.ticTacToe.board))
-    const [playerOneTurn, setPlayerOneTurn] = useState(true)
-    console.log('board props', props)
-    console.log('board i', boardInfo)
+  const [boardInfo, setBoardInfo] = useState(
+    useSelector((state) => state.ticTacToe.board)
+  );
+  const [playerOneTurn, setPlayerOneTurn] = useState(true);
+  console.log("board props", props);
+  console.log("board i", boardInfo);
 
-    const onClick = (squareId) => {
-        props.selectSquare(squareId)
-        const tempBoard = boardInfo
-        tempBoard[squareId].value = playerOneTurn ? 'X' : 'O'
-        setBoardInfo(tempBoard)
-        setPlayerOneTurn(!playerOneTurn)
+  const onClick = (squareId) => {
+    console.log("bi", boardInfo[squareId].value);
+    const tempBoard = boardInfo;
+    if (tempBoard[squareId].value === null) {
+      tempBoard[squareId].value = playerOneTurn ? "X" : "O";
+      setBoardInfo(tempBoard);
+      setPlayerOneTurn(!playerOneTurn);
+      props.selectSquare(squareId);
     }
-    return(
-    <div className='tictactoe-board'>
-        {props.currentBoard.map(square => (
-           <div
-           className='tictactoe-square'
-           key={square.id}
-           squareId={square.id}
-           selectSquare={props.selectSquare}
-           onClick={() => {onClick(square.id)}}
-           > 
-           {square.value}
-           </div>
-        ))}
-    </div>)
-}
+  };
+  
+  return (
+    <div className="tictactoe-board">
+      {props.currentBoard.map((square) => (
+        <div
+          className="tictactoe-square"
+          key={square.id}
+          squareId={square.id}
+          selectSquare={props.selectSquare}
+          onClick={() => {
+            onClick(square.id);
+          }}
+        >
+          {square.value}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Board
+export default Board;
